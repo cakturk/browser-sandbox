7e0e97254da83881a9bbd92eaa8e44d486b40858
Cihangir Akturk <cihangir.akturk@tubitak.gov.tr>
2016-03-07 16:03:24 +0200

    A bunch of crap
    Signed-off-by: Cihangir Akturk <cihangir.akturk@tubitak.gov.tr>

--- browser-sandbox.py
+++ browser-sandbox.py
@@ -6,17 +6,24 @@ import os
 uid = None
 gid = None
 
+# args;
+# 0, path to chroot
+# 1, cmd to exec in chroot
+# 2, src dir to sync from
+# 3, dst dir to sync
 def main():
     print("py: uid: {}, euid: {}".format(os.getuid(), os.geteuid()))
-    exec_proc()
+    exec_proc('/home/cakturk/dev/containers/debian-tree-firefox',
+              'firefox')
 
-def exec_proc(args):
+def exec_proc(chroot, cmd):
     pid = os.fork()
     if pid == 0:
-        pass
+        args = ['systemd-nspawn', '--setenv=DISPLAY=:0', '-D']
+        args.extend([chroot, cmd])
+        os.execvp(args[0], args)
     else:
-        # child
-        pass
+        print os.waitpid(-1, 0)
 
 if __name__ == '__main__':
     main()
