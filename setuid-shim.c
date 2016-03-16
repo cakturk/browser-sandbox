8766f684fae3151e81341c8b114eead0712141c8
Cihangir Akturk <cihangir.akturk@tubitak.gov.tr>
2016-03-16 14:42:37 +0200

    pass cmdline args to python script
    Signed-off-by: Cihangir Akturk <cihangir.akturk@tubitak.gov.tr>

--- setuid-shim.c
+++ setuid-shim.c
@@ -1,11 +1,25 @@
+#include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	execl("/usr/bin/python", "browser-sandbox",
-	      "browser-sandbox.py", NULL);
+	const char **eargv;
+	size_t nargs;
+
+	nargs = argc + 2;
+	eargv = malloc(sizeof(char *) * nargs);
+	memcpy(eargv + 2, argv + 1, (argc - 1) * sizeof(char *));
+	eargv[0] = "browser-sandbox";
+	eargv[1] = "browser-sandbox.py";
+	eargv[nargs - 1] = NULL;
+
+	execv("/usr/bin/python", (char * const *)eargv);
 
 	/* Should never be reached, unless there is an error */
+	free(eargv);
+	perror("setuid-shim");
+
 	exit(EXIT_FAILURE);
 }
